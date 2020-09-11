852226186bd6884aebca7533a54c37118d9b25d6
Prashandip Lunghimba <prashandip2@gmail.com>
2020-09-11 15:04:20 +0545

    Updates

---
+++ precache-manifest.ad23924f054cebc832bd6ad75cda8ad3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "795cf1e125e35c0d0a9a70eb1e9fad07",
+    "url": "/prashandiptech/index.html"
+  },
+  {
+    "revision": "17fa6218abbf8352a98d",
+    "url": "/prashandiptech/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "bcbd699807678f4eb309",
+    "url": "/prashandiptech/static/css/main.e046265f.chunk.css"
+  },
+  {
+    "revision": "17fa6218abbf8352a98d",
+    "url": "/prashandiptech/static/js/2.1b190a69.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/prashandiptech/static/js/2.1b190a69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcbd699807678f4eb309",
+    "url": "/prashandiptech/static/js/main.6cdefc51.chunk.js"
+  },
+  {
+    "revision": "3f2b790bc8ae553d93b4",
+    "url": "/prashandiptech/static/js/runtime-main.1334ebd7.js"
+  },
+  {
+    "revision": "54bca98fab1fd7693e5be93ea1f3b42e",
+    "url": "/prashandiptech/static/media/about-us.54bca98f.png"
+  },
+  {
+    "revision": "e546a22b2a050bf9466522ffb4c87880",
+    "url": "/prashandiptech/static/media/android-dev.e546a22b.png"
+  },
+  {
+    "revision": "81376392c641ce217816643224991056",
+    "url": "/prashandiptech/static/media/app-dev.81376392.jpg"
+  },
+  {
+    "revision": "c7ad0c181733ca4d0a9e650459f2be38",
+    "url": "/prashandiptech/static/media/digital-marketing.c7ad0c18.png"
+  },
+  {
+    "revision": "d5f868a25cc45bcf6f3b70def73bd1f4",
+    "url": "/prashandiptech/static/media/ios-dev.d5f868a2.png"
+  },
+  {
+    "revision": "313baa75638a0a49c1f84f4f0f974fee",
+    "url": "/prashandiptech/static/media/software-dev.313baa75.jpg"
+  },
+  {
+    "revision": "c18fc5fb9d7fd6190d53b0b2cc0a6a75",
+    "url": "/prashandiptech/static/media/softwork.c18fc5fb.png"
+  },
+  {
+    "revision": "512999ef541e05c4c2163d9c69351459",
+    "url": "/prashandiptech/static/media/web-dev.512999ef.jpg"
+  }
+]);
